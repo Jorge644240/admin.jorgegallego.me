3b370a233dd892511af980bfe800e43c8adcea78
Jorge Gallego <gallegojorge908@gmail.com>
2023-01-08 09:22:02 -0500

    Updated cron jobs scripts

--- cron/clearInfoLog.job.js
+++ cron/clearInfoLog.job.js
@@ -2,6 +2,6 @@ const fs = require("node:fs/promises");
 const cron = require("node-cron");
 const path = require("node:path");
 
-module.exports = cron.schedule("/1 * * * *", async () => {
+module.exports = cron.schedule("0 8 * * *", async () => {
 	await fs.writeFile(path.join(__dirname, "..", "logs", "info.log"), ""); // Empty contents of Info level log
 });
