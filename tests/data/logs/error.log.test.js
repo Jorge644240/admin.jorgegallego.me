93e0c67495eaa5fba6aecd2840500e66da096986
Jorge Gallego <gallegojorge908@gmail.com>
2023-01-08 07:52:06 -0500

    Updated Jest scripts

--- tests/data/logs/error.log.test.js
+++ tests/data/logs/error.log.test.js
@@ -6,8 +6,3 @@ test("Logs are handled with file in 'logs/' folder", () => {
 	expect(errorLogger.transports[0]).toHaveProperty("filename");
 	expect(errorLogger.transports[0]).toHaveProperty("dirname");
 });
-
-test("In 'development' environment, logs are handled in the Console", () => {
-	const errorLogger = require("../../../data/logs/error.log");
-	expect(errorLogger.transports[1]).toHaveProperty("name", "console");
-});
