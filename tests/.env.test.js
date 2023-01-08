96b5781bf65f3c12dddb00a08984cbc0c8a862ca
Jorge Gallego <gallegojorge908@gmail.com>
2023-01-08 07:51:36 -0500

    Updated Jest script

--- tests/.env.test.js
+++ tests/.env.test.js
@@ -1,14 +1,16 @@
 test("Environment variables loaded with `require('dotenv').config()`", () => {
 	require("dotenv").config();
-	expect(process.env).toHaveProperty("COOKIE_SIGNATURE");
-	expect(process.env).toHaveProperty("JWT_SIGNATURE");
 	expect(process.env).toHaveProperty("RDS_DB_HOST");
 	expect(process.env).toHaveProperty("RDS_DB_USERNAME");
 	expect(process.env).toHaveProperty("RDS_DB_PASSWORD");
 	expect(process.env).toHaveProperty("RDS_DB_NAME");
-	expect(process.env).toHaveProperty("STRICT_TRANSPORT_SECURITY");
-	expect(process.env).toHaveProperty("X_CONTENT_TYPE_OPTIONS");
-	expect(process.env).toHaveProperty("X_XSS_PROTECTION");
-	expect(process.env).toHaveProperty("X_FRAME_OPTIONS");
-	expect(process.env).toHaveProperty("CONTENT_SECURITY_POLICY");
+	if (process.env.NODE_ENV !== "actions") {
+		expect(process.env).toHaveProperty("COOKIE_SIGNATURE");
+		expect(process.env).toHaveProperty("JWT_SIGNATURE");
+		expect(process.env).toHaveProperty("STRICT_TRANSPORT_SECURITY");
+		expect(process.env).toHaveProperty("X_CONTENT_TYPE_OPTIONS");
+		expect(process.env).toHaveProperty("X_XSS_PROTECTION");
+		expect(process.env).toHaveProperty("X_FRAME_OPTIONS");
+		expect(process.env).toHaveProperty("CONTENT_SECURITY_POLICY");
+	}
 });
