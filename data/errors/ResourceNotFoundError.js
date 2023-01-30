20777a21cc922c7599c6bbb69bd172076ed3b92c
Jorge Gallego <gallegojorge908@gmail.com>
2023-01-30 13:11:03 -0500

    Custom Error classes for in-app use

--- data/errors/ResourceNotFoundError.js
+++ data/errors/ResourceNotFoundError.js
@@ -1,4 +1,10 @@
 module.exports = class ResourceNotFoundError extends Error {
+	/**
+	 * @constructs ResourceNotFoundError
+	 * @param {object} config - ResourceNotFoundError configuration options
+	 * @param {string} config.resourceType - DB resource intended for search
+	 * @param {object} config.searchParameters - Attributes used in unsuccessful search 
+	 */
 	constructor(config) {
 		super("The specified resource doesn't exist");
 		this.name = "ResourceNotFoundError";
